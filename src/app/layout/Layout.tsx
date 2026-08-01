37b1772646860c2201477bc32713b2263976b6ae
noreo <abercon@gmail.com>
2026-08-01 10:23:16 -0400

    fixed horizontal scroll

--- src/app/layout/Layout.tsx
+++ src/app/layout/Layout.tsx
@@ -5,7 +5,7 @@ import { ScrollToTop } from "../components/ScrollToTop";
 
 export default function Layout() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen overflow-x-hidden">
       <ScrollToTop />
       <Header />
       <main className="flex-grow">
